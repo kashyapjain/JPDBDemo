78fbf124265fc9bb063f45a04d0dcdb4b532fcdc
kashyapjain <kashyapjain2018@gmail.com>
2022-11-20 15:42:48 +0530

    Create Put User

---
+++ PutUser.js
@@ -0,0 +1,48 @@
+const TOKEN = "90938061|-31949266237856268|90952960";
+const DB = "Users"
+const REL = "User-Rel"
+
+function validateAndGetFormData() {
+    var nameInput = document.getElementById("inputName");
+    var nameValue = nameInput.value;
+
+    if (nameValue == "") {
+        alert("Name Requiered");
+        nameInput.focus();
+        return "";
+    }
+
+    var emailInput = document.getElementById("inputEmail");
+    var emailValue = emailInput.value;
+
+    if (emailValue == "") {
+        alert("Email Requiered");
+        emailInput.focus();
+        return "";
+    }
+
+    var jsonObj = {
+        name: nameValue,
+        email: emailValue
+    };
+
+    return JSON.stringify(jsonObj);
+}
+
+function saveUser() {
+    var jsonStr = validateAndGetFormData();
+
+    if (jsonStr == "") {
+        return;
+    }
+
+    var reqStr = createPUTRequest(TOKEN, jsonStr, DB, REL);
+
+    var endPoint = "/api/iml";
+
+    jQuery.ajaxSetup({ async: false });
+    var res = executeCommand(reqStr, endPoint);
+    jQuery.ajaxSetup({ async: true });
+
+    alert(JSON.stringify(res));
+}
